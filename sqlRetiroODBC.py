7e3d166fbd78fb44912d096c9037bb5438833dbd
imenezes4413 <134085524+imenezes4413@users.noreply.github.com>
2024-01-29 12:07:39 -0300

    Atualização
    Atualização

--- sqlRetiroODBC.py
+++ sqlRetiroODBC.py
@@ -1,22 +1,11 @@
 import pyodbc
 import mysql.connector as mysql
+from datetime import datetime
 
-class Credentials():
-    def __init__(self) -> None:
-        self.__server = None
-        self.__database = None
-        self.__password = None
-        self.__username = None
-        self.__command = None
-        self.__portNumber = '1433'
-
-
-
-class ServerRetiroPbs(Credentials):
+class ServerRetiroPbs():
     def __init__(self) -> None:
         self.__connectionString = None
         self.__cursor = None
-        self.__return = None
         self.__portNumber = '1433'
 
     #Configuração Conexão
@@ -36,58 +25,93 @@ class ServerRetiroPbs(Credentials):
         self.__command = command 
 
     #Retornos conexão
-    def getUserValidation(self):
+    def getValidation(self):
         self.__connectionString = f'DRIVER={{SQL Server}};SERVER={self.__server},{self.__portNumber};DATABASE={self.__database};UID={self.__username};PWD={self.__password}'
         self.__conn = pyodbc.connect(self.__connectionString)
         self.__cursor = self.__conn.cursor()
         self.__cursor.execute(self.__command)
-        teste = self.__cursor.fetchall()
-        print(teste)
+        usuario = self.__cursor.fetchall()
 
-        if len(teste)>0 :
-            return True
-        
+        if usuario:
+            retornoBanco = usuario[0][0]
+            return retornoBanco
         else:
-            return False
+            return False         
+        
+    def stringPBS(self):
+        self.__connectionString = f'DRIVER={{SQL Server}};SERVER={self.__server},{self.__portNumber};DATABASE={self.__database};UID={self.__username};PWD={self.__password}'
+        self.__conexao = pyodbc.connect(self.__connectionString)
+        cursor = self.__conexao.cursor()
+        cursor.execute(self.__command)
+        resultado = cursor.fetchall()
+        
+        return resultado
+    
+    def insertPBS(self):
+        print('inicio conexão banco de dados SQL Server BD_Python')
+        self.__connectionString = f'DRIVER={{SQL Server}};SERVER={self.__server},{self.__portNumber};DATABASE={self.__database};UID={self.__username};PWD={self.__password}'
+        self.__conexao = pyodbc.connect(self.__connectionString)
+        cursor = self.__conexao.cursor()
+        print('executando o comando')
+        cursor.execute(self.__command)
+        print('commintando o comando')
+        cursor.commit()
+        
 
 
 
-class serverGlpi(Credentials):
+class serverGlpi():
 
     def __init__(self) -> None:
-        self.__ticket = None
-        self.__groupTicket = None
-        self.__solveDate = None
+        self.__username = None
+        self.__password = None
         self.__server = None
+        self.__database =None
+        self.__command= None
+        
         
         
     def setServer(self, server):
         self.__server = server
 
-    def setUser(self, user):
-        self.__username = user
-
-    def setDatabase(self, database):
-        self.__database = database
+    def setUser(self, usuario):
+        self.__username = usuario
     
     def setPassword(self, password):
         self.__password = password
-
+    
+    def setDatabase(self, database):
+        self.__database = database 
+    
     def setCommand(self, command):
-        self.__command = command 
-    
-    
-    def getTicket(self,ticket):
-        self.__ticket = ticket
+        self.__command=command
 
-    def returnGLPI(self):
-        conexao = mysql.connect(host=self.__server, database=self.__database, user=self.__username, password=self.__password)
+
+    def stringGLPI(self):
+        conexao = mysql.connect(host=f'{self.__server}', database=f'{self.__database}',user=f'{self.__username}', password=f'{self.__password}')
         cursor = conexao.cursor()
         cursor.execute(self.__command)
-        retorno = cursor.fetchall()[0][0]
-        if retorno != None:
-            return True
-        elif retorno =='':
-            return False
+        retorno = cursor.fetchall()
+        print(retorno)
+        if retorno:
+            retornoBanco = retorno[0][0]
+            return retornoBanco
         else:
             return False
+
+           
+
+
+
+        
+        
+
+
+
+    
+        
+
+
+        
+        
+        
