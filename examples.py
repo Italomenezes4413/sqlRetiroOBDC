7279a29e184c05823acec6ae7f78619eb2a9b14e
Italomenezes4413 <74031259+Italomenezes4413@users.noreply.github.com>
2025-08-14 15:05:37 -0300

    fs
    sd

--- examples.py
+++ examples.py
@@ -1,6 +1,6 @@
 import sqlRetiroODBC as sql
 from icecream import ic
-# get IP of de local machine
+# get IP of the local machine
 # info = sql.informacoesMaquinas()
 # info = informacoes.getAllInfo()
 
