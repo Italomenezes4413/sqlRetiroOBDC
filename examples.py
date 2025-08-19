22b81d7aecacd8cb79554dbeb638b7fa1811f2ed
Italomenezes4413 <74031259+Italomenezes4413@users.noreply.github.com>
2025-08-19 13:52:14 -0300

    Update
    Carregando as configuraões do meu vs code por exemplo

---
+++ examples.py
@@ -0,0 +1,25 @@
+{
+    "workbench.colorTheme": "Dracula Theme",
+    "workbench.colorCustomizations": {
+        "editor.background": "#000000",
+        "sideBar.background": "#000000",
+        "activityBar.background": "#000000",
+        "terminal.background": "#000000",
+        "statusBar.background": "#000000",
+        "titleBar.activeBackground": "#000000"
+    },
+    "python.createEnvironment.trigger": "off",
+    "security.workspace.trust.untrustedFiles": "open",
+    "explorer.confirmDelete": false,
+    "github.copilot.enable": {
+        "python": false,
+        "*": false
+    },
+    "explorer.confirmDragAndDrop": false,
+    "github.copilot.advanced": {
+
+    },
+    "editor.fontLigatures": true,
+    "editor.fontFamily": "Fira code",
+    "editor.fontWeight":"500" 
+}
